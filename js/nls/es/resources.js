e173ffe58275b851cf5e40fa9833cc641856863f
Javier Abadia <javier.abadia@esri.es>
2014-09-08 13:48:40 +0200

    primera versión que funciona

---
+++ js/nls/es/resources.js
@@ -0,0 +1,75 @@
+﻿({
+  embedViewer: {
+    noAccess: "No tienes acceso a este mapa Web.",
+    mapNotAvailable: "El mapa no está disponible."
+  },
+  presentation:{
+    title: "Presentación",
+    first: "Primera",
+    previous: "Anterior",
+    start: "Inicio",
+    next: "Siguiente",
+    last: "Última",
+    of: "de",
+    slideLabel: "Diapositiva:",
+    errors:{
+      private: "No se puede ver la presentación del mapa. El mapa no se está compartiendo actualmente con Todos (público).",
+      notFound: "La presentación del mapa no está disponible."
+    }
+  },
+  configure: {
+    layerAndFields:{
+        layer: "Seleccionar capa",
+        field: "Seleccionar campo"
+    },
+    mapdlg:{
+       items:{
+        organizationLabel: "Mi organización",
+        onlineLabel: "ArcGIS Online",
+        contentLabel: "Mi contenido",
+        favoritesLabel: "Mis favoritos"
+      },
+      title: "Seleccionar mapa web",
+      searchTitle: "Buscar",
+      ok: "Aceptar",
+      cancel: "Cancelar",
+      placeholder: "Introducir término de búsqueda"
+    },
+    groupdlg:{
+      items:{
+        organizationLabel: "Mi organización",
+        onlineLabel: "ArcGIS Online",
+        contentLabel: "Mi contenido",
+        favoritesLabel: "Mis favoritos"
+      },
+      title: "Seleccionar grupo",
+      searchTitle: "Buscar",
+      ok: "Aceptar",
+      cancel: "Cancelar",
+      placeholder: "Introducir término de búsqueda"
+    },
+    itemMessage: "Mostrando actualmente ${title} ${type}. Utiliza el botón ${label} para elegir un ${type} diferente.",
+    noItemMessage: "Utiliza el botón ${label} para seleccionar un ${type} que desees mostrar.",
+    title: "Configurar aplicación Web",
+    save:{
+      label: "Guardar",
+      tooltip: "Guardar"
+    },
+    refresh: "Actualizar",
+    reset: {
+      label: "Reiniciar",
+      tooltip: "Reiniciar"
+    },
+    cancel:{
+      label: "Cancelar",
+      tooltip: "Cancelar e ir a los detalles del elemento."
+    },
+    done: "Hecho",
+    errors:{
+      noFields: "No se ha encontrado ningún campo para la capa seleccionada.",
+      error: "Se ha producido un error.",
+      notConfigurable: "Esta plantilla no puede configurarse.",
+      notAvailable: "La aplicación no está disponible."
+    }
+  }
+})
---
+++ js/nls/resources.js
@@ -0,0 +1,75 @@
+﻿({
+  embedViewer: {
+    noAccess: "You do not have access to this Web Map.",
+    mapNotAvailable: "The map is not available."
+  },
+  presentation:{
+    title: "Presentation",
+    first: "First",
+    previous: "Previous",
+    start: "Start",
+    next: "Next",
+    last: "Last",
+    of: "of",
+    slideLabel: "Slide:",
+    errors:{
+      private: "Unable to view map presentation.  The map is not currently shared with Everyone (public).",
+      notFound: "The map presentation is not available."
+    }
+  },
+  configure: {
+    layerAndFields:{
+        layer: "Select layer",
+        field: "Select field"
+    },
+    mapdlg:{
+       items:{
+        organizationLabel: "My Organization",
+        onlineLabel: "ArcGIS Online",
+        contentLabel: "My Content",
+        favoritesLabel: "My Favorites"
+      },
+      title: "Select Web Map",
+      searchTitle: "Search",
+      ok: "Ok",
+      cancel: "Cancel",
+      placeholder: "Enter search term"
+    },
+    groupdlg:{
+      items:{
+        organizationLabel: "My Organization",
+        onlineLabel: "ArcGIS Online",
+        contentLabel: "My Content",
+        favoritesLabel: "My Favorites"
+      },
+      title: "Select Group",
+      searchTitle: "Search",
+      ok: "Ok",
+      cancel: "Cancel",
+      placeholder: "Enter search term"
+    },
+    itemMessage: "Currently displaying ${title} ${type}. Use the ${label} button to choose a different ${type}.",
+    noItemMessage: "Use the ${label} button to select a ${type} to display.",
+    title: "Configure Web Application",
+    save:{
+      label: "Save",
+      tooltip: "Save"
+    },
+    refresh: "Refresh",
+    reset: {
+      label: "Reset",
+      tooltip: "Reset"
+    },
+    cancel:{
+      label: "Cancel",
+      tooltip: "Cancel and go to item details."
+    },
+    done: "Done",
+    errors:{
+      noFields: "No fields found for the selected layer.",
+      error: "There was an error.",
+      notConfigurable: "This template is not configurable.",
+      notAvailable: "The application is not available."
+    }
+  }
+})
